e99daa3a11ee5eefd9adbeae3423e1f61842b1b8
robin <robin.greig@calalta.com>
2019-10-23 12:27:43 -0600

    updated allFix.sh

--- allFix.sh
+++ allFix.sh
@@ -3,9 +3,9 @@
 # Connect to Ethernet before running so that it can access updates
 
 ## Add SAIT IP addresses for NTP servers
-#sudo cp -f /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.bak
-#sudo bash -c "echo 'NTP= 10.197.2.9 10.197.3.9' >> /etc/systemd/timesyncd.conf"
-##sudo timedatectl set-ntp true
+sudo cp -f /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.bak
+sudo bash -c "echo 'NTP= 10.197.2.9 10.197.3.9' >> /etc/systemd/timesyncd.conf"
+#sudo timedatectl set-ntp true
 
 # Set current time
 read -p 'Current Date YYYY/MM/DD: ' datevar
@@ -17,14 +17,14 @@ sudo date -s $timevar
 sudo apt update && sudo apt upgrade -y
 
 # Temporary fix for WPA Enterprise on Raspbian Buster
-#sudo apt-get remove wpasupplicant -y
-#sudo mv -f /etc/apt/sources.list /etc/apt/sources.list.bak
-#sudo bash -c "echo 'deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.list"
-#sudo apt-get update
-#sudo apt-get install wpasupplicant -y
-#sudo apt-mark hold wpasupplicant
-#sudo cp -f /etc/apt/sources.list.bak /etc/apt/sources.list
-#sudo apt-get update
+sudo apt remove wpasupplicant -y
+sudo mv -f /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo bash -c "echo 'deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.list"
+sudo apt update
+sudo apt install wpasupplicant -y
+sudo apt-mark hold wpasupplicant
+sudo cp -f /etc/apt/sources.list.bak /etc/apt/sources.list
+sudo apt update
 
 ## Update wpa_supplicant file
 sudo cp -f /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
@@ -49,3 +49,7 @@ sudo bash -c "echo '     password=\"$passvar\"' >> /etc/wpa_supplicant/wpa_suppl
 sudo bash -c "echo '     phase1=\"peaplabel=0\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
 sudo bash -c "echo '     phase2=\"auth=MSCHAPV2\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
 sudo bash -c "echo '}' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+
+# Reboot system for changes to take place
+sudo reboot
+
