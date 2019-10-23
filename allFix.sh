d35a54a580893f24d5cb43795edc75e78df3e997
Robin <robin.greig@calalta.com>
2019-10-23 10:28:30 -0600

    updated allFix.sh

--- allFix.sh
+++ allFix.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+
+# Connect to Ethernet before running so that it can access updates
+
 ## Add SAIT IP addresses for NTP servers
 #sudo cp -f /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.bak
 #sudo bash -c "echo 'NTP= 10.197.2.9 10.197.3.9' >> /etc/systemd/timesyncd.conf"
 ##sudo timedatectl set-ntp true
 
-## Set current time
-#sudo date -s 2019/10/21
-#sudo date -s 17:00
+# Set current time
+read -p 'Current Date YYYY/MM/DD: ' datevar
+sudo date -s $datevar
+read -p 'Current Time HH:MM: ' timevar
+sudo date -s $timevar
 
-## Temporary fix for WPA Enterprise on Raspbian Buster, 
-## connect Ethernet before running
+# Update && Upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Temporary fix for WPA Enterprise on Raspbian Buster
 #sudo apt-get remove wpasupplicant -y
 #sudo mv -f /etc/apt/sources.list /etc/apt/sources.list.bak
 #sudo bash -c "echo 'deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.list"
@@ -18,24 +25,27 @@
 #sudo apt-mark hold wpasupplicant
 #sudo cp -f /etc/apt/sources.list.bak /etc/apt/sources.list
 #sudo apt-get update
-#sudo mv -f /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
-#sudo cp -f ./wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
-## Updat wpa_supplicant file
-sudo bash -c "echo 'country=CA' > /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo 'update_config=1' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo 'network={' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     ssid=\"sait-secure\"' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     priority=1' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     proto=RSN' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     key_mgmt=WPA-EAP' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     pairwise=CCMP' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     auth_alg=OPEN' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     eap=PEAP' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     identity=\"serv16makerspace01\"' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     password=\"MkSP@1601\"' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     phase1=\"peaplabel=0\"' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '     phase2=\"auth=MSCHAPV2\"' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
-sudo bash -c "echo '}' >> /etc/wpa_supplicant/wpa_supplicant.conf.test"
+## Update wpa_supplicant file
+sudo cp -f /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
+sudo bash -c "echo 'country=CA' > /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo 'update_config=1' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo 'network={' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     ssid=\"sait-secure\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     priority=1' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     proto=RSN' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     key_mgmt=WPA-EAP' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     pairwise=CCMP' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     auth_alg=OPEN' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     eap=PEAP' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+read -p 'SAIT Username: ' uservar
+sudo bash -c "echo '     identity=\"$uservar\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+#sudo bash -c "echo '     identity=\"serv16makerspace01\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+read -p 'SAIT Password: ' passvar
+sudo bash -c "echo '     password=\"$passvar\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+#sudo bash -c "echo '     password=\"MkSP@1601\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     phase1=\"peaplabel=0\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '     phase2=\"auth=MSCHAPV2\"' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+sudo bash -c "echo '}' >> /etc/wpa_supplicant/wpa_supplicant.conf"
